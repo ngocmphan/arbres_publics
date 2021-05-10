b74c5a086bf6bb19568224840f691409a235cd56
ngocmphan <ngocm.phan@gmail.com>
2021-05-09 23:02:15 -0400

    #Please enter the commit message for your changes. Lines starting Explored Side column

--- data_prep.py
+++ data_prep.py
@@ -1,6 +1,21 @@
-import pandas 
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from bokeh.io import output_file, show
+from bokeh.plotting import figure
 
-
-trees = pandas.read_csv("arbres-publics.csv", dtype={'ARROND_NOM': str, 'Rue': str}, low_memory=False)
+trees = pd.read_csv("arbres-publics.csv", dtype={'ARROND_NOM': str, 'Rue': str}, low_memory=False)
+trees['count'] = 1
 
 print(trees.info())
+print(trees['COTE'].unique())
+
+cote_count = pd.pivot_table(trees, values='count', index='COTE', aggfunc=np.sum)
+print(cote_count)
+
+p = figure(plot_height=250, title="Side count",
+           toolbar_location=None, tools="")
+
+p.vbar(x=cote_count.index, top=cote_count['count'], width=0.9)
+
+show(p)
