a3dcea17dc1f55573b6f98147d46a982e814fa92
ngocmphan <ngocm.phan@gmail.com>
2021-05-14 22:49:44 -0400

    Explore trees name and areas name.

--- data_prep.py
+++ data_prep.py
@@ -3,4 +3,7 @@ import pandas
 
 trees = pandas.read_csv("arbres-publics.csv", dtype={'ARROND_NOM': str, 'Rue': str}, low_memory=False)
 
-print(trees.info())
+#print(trees.info())
+print(trees['ARROND_NOM'].value_counts())
+print(trees['ESSENCE_ANG'].value_counts())
+print(trees['ESSENCE_ANG'].nunique())
