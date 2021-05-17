73ad30f41a81cac6955f467a83f59d17e7b976cf
ngocmphan <ngocm.phan@gmail.com>
2021-05-16 23:44:58 -0400

    Lower case for place variable. Created a new variable with reduced unique observations.

--- data_prep.py
+++ data_prep.py
@@ -10,5 +10,18 @@ trees = pd.read_csv("arbres-publics.csv", dtype={'ARROND_NOM': str, 'Rue': str},
 trees['DHP'] = trees['DHP'].fillna((trees['DHP'].mean()))
 print(trees.info())
 
-print(trees['ARROND_NOM'].value_counts())
-print(trees['ESSENCE_ANG'].value_counts())
+
+# Variable transformation
+trees['Emplacement'] = trees['Emplacement'].str.lower()
+
+
+def place_col(df):
+    df['place'] = np.where(df['Emplacement'].str.contains('parterre'), 'parterre', 'other')
+    return df
+
+
+trees_new = place_col(trees)
+
+print(trees_new['place'].value_counts())
+print(trees['Emplacement'].value_counts())
+
