db67faf643e40b22ab6cee7ce2d5f3eb80b2e453
ngocmphan <ngocm.phan@gmail.com>
2021-05-16 22:39:11 -0400

    Replaced missing values with mean: DHP

--- data_prep.py
+++ data_prep.py
@@ -6,8 +6,9 @@ from bokeh.plotting import figure
 
 trees = pd.read_csv("arbres-publics.csv", dtype={'ARROND_NOM': str, 'Rue': str}, low_memory=False)
 
-# print(trees.info())
+# Missing values handling
+trees['DHP'] = trees['DHP'].fillna((trees['DHP'].mean()))
+print(trees.info())
+
 print(trees['ARROND_NOM'].value_counts())
 print(trees['ESSENCE_ANG'].value_counts())
-print(trees['ESSENCE_ANG'].nunique())
-
