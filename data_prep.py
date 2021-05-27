f566bd5c2ce6be76f990c865ccf10157c1eec89f
ngocmphan <ngocm.phan@gmail.com>
2021-05-26 20:35:12 -0400

    Explored place variable

--- data_prep.py
+++ data_prep.py
@@ -3,7 +3,7 @@ import pandas
 
 trees = pandas.read_csv("arbres-publics.csv", dtype={'ARROND_NOM': str, 'Rue': str}, low_memory=False)
 
-#print(trees.info())
-print(trees['ARROND_NOM'].value_counts())
-print(trees['ESSENCE_ANG'].value_counts())
-print(trees['ESSENCE_ANG'].nunique())
+print(trees.info())
+print(trees['Emplacement'].nunique())
+print(trees['Emplacement'].value_counts())
+
