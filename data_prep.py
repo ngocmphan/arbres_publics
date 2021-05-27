02306ba71843a6f359143808105c3db5263456ee
ngocmphan <ngocm.phan@gmail.com>
2021-05-26 21:07:00 -0400

    Added responses to questions.

--- data_prep.py
+++ data_prep.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import matplotlib.pyplot as plt
 import numpy as np
 
 
@@ -44,6 +43,22 @@ trees['DHP'] = trees['DHP'].fillna((trees['DHP'].mean()))
 trees['Emplacement'] = trees['Emplacement'].str.lower()
 trees_new = place_col(trees)
 
-print(trees.info())
+### Questions
+
+##1 Comparison between on-road and off-road trees:
+no_on_off_road = trees_new['INV_TYPE'].value_counts()
+
+##2 Top 3 areas with most trees
+areas = trees_new['ARROND_NOM'].value_counts()
+
+##3 Top 3 most planted trees
+trees_type = trees_new['ESSENCE_ANG'].value_counts()
+
+##4 Type of earth where trees are most planted
+earth_type = trees_new['place'].value_counts()
+print(earth_type)
+print(trees_new.info())
+
+
 
 
