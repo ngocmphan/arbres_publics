2413598c310a95f3294a91c50dff12d15700a45d
ngocmphan <ngocm.phan@gmail.com>
2021-06-01 12:24:08 -0400

    Optimized map codes.

--- visual_deliverables.py
+++ visual_deliverables.py
@@ -7,13 +7,11 @@ trees_new = trees_new.dropna(subset=['Longitude', 'Latitude'], axis=0)
 
 
 # Create Map
-m = folium.Map(location=[45.513, -73.612], zoom_start=10,
+m = folium.Map(location=[45.513, -73.612], zoom_start=11,
                titles='Montreal maps of trees')
-
-tooltip = "I'm a tree"
 
 for index, row in trees_new.iterrows():
     location = [row['Latitude'], row['Longitude']]
-    folium.Marker(location, tooltip=tooltip).add_to(m)
+    folium.Marker(location).add_to(m)
 
 m.save(outfile='maps_of_trees.html')
