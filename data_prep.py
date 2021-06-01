1e258d984a37fab7ba3a81d2d340234d91b19f74
ngocmphan <ngocm.phan@gmail.com>
2021-05-31 22:55:28 -0400

    Removed deliverable section in data_prep.

--- data_prep.py
+++ data_prep.py
@@ -37,21 +37,6 @@ trees['DHP'] = trees['DHP'].fillna((trees['DHP'].mean()))
 trees['Emplacement'] = trees['Emplacement'].str.lower()
 trees_new = place_col(trees)
 
-# Questions
-
-# 1 Comparison between on-road and off-road trees:
-no_on_off_road = trees_new['INV_TYPE'].value_counts()
-
-# 2 Top 3 areas with most trees
-areas = trees_new['ARROND_NOM'].value_counts()
-
-# 3 Top 3 most planted trees
-trees_type = trees_new['ESSENCE_ANG'].value_counts()
-
-# 4 Type of earth where trees are most planted
-earth_type = trees_new['place'].value_counts()
-print(earth_type)
-print(trees_new.info())
 
 
 
