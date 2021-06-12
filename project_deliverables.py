5817159acd88dc1a4bf553f2f4d4bfb4fe5238a4
ngocmphan <ngocm.phan@gmail.com>
2021-06-12 15:36:52 -0400

    Added boxplot of trees diameters by area. (Deliverable 5)

--- project_deliverables.py
+++ project_deliverables.py
@@ -1,4 +1,6 @@
 from data_prep import trees_new
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 # Questions
 
@@ -16,3 +18,7 @@ earth_type = trees_new['place'].value_counts()
 print(earth_type)
 print(trees_new.info())
 
+# 5 Compare the DHP of trees in different areas
+box_plot = trees_new.boxplot(column=['DHP'], by=['ARROND_NOM'], fontsize=8)
+ax = sns.boxplot(x='DHP', y='ARROND_NOM', data=trees_new)
+plt.show()
