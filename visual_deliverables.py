a87c2550b0d216dbc2784a9896981b7fc026aae5
ngocmphan <ngocm.phan@gmail.com>
2021-09-27 15:13:18 -0400

    Editted map start location.

--- visual_deliverables.py
+++ visual_deliverables.py
@@ -3,17 +3,20 @@ import folium
 import pandas as pd
 print(trees_new['ARROND_NOM'].unique())
 # Excluding NaN rows
-trees_borough = trees_new[trees_new['ARROND_NOM'] == 'Rosemont - La Petite-Patrie']
+trees_borough = trees_new[trees_new['ARROND_NOM'] ==
+                          'Rosemont - La Petite-Patrie']
 trees_road = trees_borough[trees_borough['INV_TYPE'] == 'R']
-trees_viz = trees_road.dropna(subset=['Longitude', 'Latitude'], axis=0)
-
+trees_placement = trees_road[trees_road['place'] == 'banquette']
+trees_viz = trees_placement.dropna(subset=['Longitude', 'Latitude'], axis=0)
+trees_viz = trees_viz[['Longitude', 'Latitude']]
 
 # Create Map
-m = folium.Map(location=[45.513, -73.612], zoom_start=11,
+m = folium.Map(location=[45.588457730918, -73.56167346642063], zoom_start=15,
                titles='Montreal maps of trees')
 
 for index, row in trees_viz.iterrows():
     location = [row['Latitude'], row['Longitude']]
-    folium.Marker(location).add_to(m)
+    folium.Marker(location, tooltip=location).add_to(m)
+
 
 m.save(outfile='maps_of_trees.html')
