00f8e10d9c80cb8f5cd4b832f3ff3c3ba653e0fb
ngocmphan <ngocm.phan@gmail.com>
2021-10-01 14:47:36 -0400

    Added viz_map_option function.

--- visual_deliverables.py
+++ visual_deliverables.py
@@ -1,28 +1,17 @@
 from data_prep import trees_new
 import folium
 import pandas as pd
-print(trees_new['ARROND_NOM'].unique())
-
-# Selecting vars
-trees_borough = trees_new[trees_new['ARROND_NOM'] ==
-                          'Rosemont - La Petite-Patrie']
-trees_road = trees_borough[trees_borough['INV_TYPE'] == 'R']
-trees_placement = trees_road[trees_road['place'] == 'parc']
-
-# Drop NaN
-trees_viz = trees_placement.dropna(subset=['Longitude', 'Latitude'], axis=0)
-trees_viz = trees_viz[['Longitude', 'Latitude']]
-
-# Create Map
-m = folium.Map(location=[45.588457730918, -73.56167346642063], zoom_start=15,
-               titles='Montreal maps of trees')
-
-for index, row in trees_viz.iterrows():
-    location = [row['Latitude'], row['Longitude']]
-    folium.Marker(location, tooltip=location).add_to(m)
 
 
-m.save(outfile='maps_of_trees.html')
+def viz_map_option(item):
+    if 'borough' == item:
+        print(trees_new['ARROND_NOM'].unique())
+    elif 'place' == item:
+        print(trees_new['place'].unique())
+    else:
+        print('Boroughs', trees_new['ARROND_NOM'].unique())
+        print('Placement', trees_new['place'].unique())
+    return
 
 
 def viz_map(borough, inv, place):
@@ -59,3 +48,6 @@ def viz_map(borough, inv, place):
 
     m.save(outfile=''.format(borough, inv, place))
     return
+
+
+
