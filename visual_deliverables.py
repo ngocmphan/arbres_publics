dfbddf0d7597987991677914f8460af5af1736d4
ngocmphan <ngocm.phan@gmail.com>
2022-12-11 00:04:44 -0500

    Added Choropleth plotting.

--- visual_deliverables.py
+++ visual_deliverables.py
@@ -14,9 +14,7 @@ def viz_map_option(item):
     return
 
 
-viz_map_option("a")
-
-path = "limites-administratives-agglomeration-nad83.geojson"
+path = "limites-administratives-agglomeration-nad83.json"
 
 gdf = geopandas.read_file(path)
 print(gdf.columns)
@@ -40,5 +38,27 @@ borough_names_adjusted = ['Ahuntsic-Cartierville',
 trees_choropleth = trees_new.replace(borough_names_to_replace,
                                      borough_names_adjusted)
 
+top_10_trees = list(trees_new['ESSENCE_ANG'].value_counts()[:10].index)
+print(top_10_trees)
 
+type_selected = ["Silver Maple"]
+trees_choropleth = trees_choropleth[trees_choropleth["ESSENCE_ANG"]
+    .isin(type_selected)]
+viz_choropleth = trees_choropleth.groupby(["ARROND_NOM"], as_index=False).count()
 
+print(viz_choropleth)
+print(viz_choropleth.columns)
+print(viz_choropleth[['ARROND_NOM', "INV_TYPE"]])
+
+m = folium.Map(location=[45.50, -73.62], zoom_start=3)
+folium.Choropleth(
+    geo_data=open(path).read(),
+    data=viz_choropleth,
+    columns=['ARROND_NOM', 'INV_TYPE'],
+    key_on="features.properties.NOM",
+    fill_color='YlOrRd',
+    fill_opacity=0.7,
+    line_opacity=0.2,
+    legend_name='SF Crime').add_to(m)
+
+m.save("Choropleth of {} on Montreal Island".format(type_selected))
