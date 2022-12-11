7966feb496f91346455f827460765fbcfb47efa5
ngocmphan <ngocm.phan@gmail.com>
2022-12-11 00:40:39 -0500

    Adjusted Choropleth plot.

--- visual_deliverables.py
+++ visual_deliverables.py
@@ -17,8 +17,7 @@ def viz_map_option(item):
 path = "limites-administratives-agglomeration-nad83.json"
 
 gdf = geopandas.read_file(path)
-print(gdf.columns)
-print(gdf["NOM_OFFICIEL"].unique())
+# print(gdf["NOM_OFFICIEL"].unique())
 
 borough_names_to_replace = list(trees_new['ARROND_NOM'].unique())
 
@@ -39,26 +38,34 @@ trees_choropleth = trees_new.replace(borough_names_to_replace,
                                      borough_names_adjusted)
 
 top_10_trees = list(trees_new['ESSENCE_ANG'].value_counts()[:10].index)
-print(top_10_trees)
 
 type_selected = ["Silver Maple"]
 trees_choropleth = trees_choropleth[trees_choropleth["ESSENCE_ANG"]
     .isin(type_selected)]
 viz_choropleth = trees_choropleth.groupby(["ARROND_NOM"], as_index=False).count()
+viz_choropleth = viz_choropleth[['ARROND_NOM', "INV_TYPE"]]
 
-print(viz_choropleth)
-print(viz_choropleth.columns)
-print(viz_choropleth[['ARROND_NOM', "INV_TYPE"]])
+list_not_in = []
+for value in gdf['NOM_OFFICIEL']:
+    if value in borough_names_adjusted:
+        None
+    else:
+        list_not_in.append(value)
 
-m = folium.Map(location=[45.50, -73.62], zoom_start=3)
+print(list_not_in)
+
+df_new = pd.DataFrame({"ARROND_NOM": list_not_in, "INV_TYPE": 0})
+viz_choropleth = viz_choropleth.append(df_new).reset_index()
+
+m = folium.Map(location=[45.50, -73.62], zoom_start=5)
 folium.Choropleth(
     geo_data=open(path).read(),
     data=viz_choropleth,
     columns=['ARROND_NOM', 'INV_TYPE'],
-    key_on="features.properties.NOM",
+    key_on="feature.properties.NOM_OFFICIEL",
     fill_color='YlOrRd',
     fill_opacity=0.7,
     line_opacity=0.2,
-    legend_name='SF Crime').add_to(m)
+    legend_name='Number of trees').add_to(m)
 
-m.save("Choropleth of {} on Montreal Island".format(type_selected))
+m.save("Choropleth of {} on Montreal Island.html".format(type_selected))
