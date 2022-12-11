5943bc913b70d572dce00323eab4305248e1363e
ngocmphan <ngocm.phan@gmail.com>
2022-12-10 22:53:07 -0500

    Adjusted borough names for geomaping.

--- visual_deliverables.py
+++ visual_deliverables.py
@@ -22,3 +22,23 @@ gdf = geopandas.read_file(path)
 print(gdf.columns)
 print(gdf["NOM_OFFICIEL"].unique())
 
+borough_names_to_replace = list(trees_new['ARROND_NOM'].unique())
+
+borough_names_adjusted = ['Ahuntsic-Cartierville',
+                          'Villeray–Saint-Michel–Parc-Extension',
+                          'Rosemont–La Petite-Patrie',
+                          'Mercier–Hochelaga-Maisonneuve',
+                          'Le Plateau-Mont-Royal',
+                          'Ville-Marie',
+                          'Côte-des-Neiges–Notre-Dame-de-Grâce',
+                          'Le Sud-Ouest',
+                          'Rivière-des-Prairies–Pointe-aux-Trembles',
+                          'Saint-Léonard',
+                          'LaSalle', 'Verdun', 'Pierrefonds-Roxboro',
+                          'Saint-Laurent', 'Anjou', 'Montréal-Est']
+
+trees_choropleth = trees_new.replace(borough_names_to_replace,
+                                     borough_names_adjusted)
+
+
+
